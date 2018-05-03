bef5408ecd3cdfc6816e419007aa4875e374ee01
kernyden <kernyden@gmail.com>
2018-05-03 23:22:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
+let port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/dist'));
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up!');
 })
